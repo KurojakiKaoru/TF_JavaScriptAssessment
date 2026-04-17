142d90f96c9150fe180a4ead63a41ab1e4445e1c
Leron Hinds <leronhinds@gmail.com>
2026-04-17 12:08:07 -0400

    Task 2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,12 @@
-function fizzBuzz(start, end, skip){
-    // Your code here!
+function fizzBuzz(start, end, skip) {
+    const fizzyBuzzy = [];
+
+    for(let i = start; i <= end; i++) {
+        if(i.toString().includes(skip)) continue;
+        else fizzyBuzzy.push(i);
+    }
+
+    return fizzyBuzzy;
 }
 
 // DON'T EDIT THE LINE BELOW THIS COMMENT
